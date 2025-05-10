4bc3446eb6eddb2850931dfb8357d1e896684c84
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2025-05-10 19:48:59 +0100

    react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import router from "./routing/routes";
 import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -14,7 +16,8 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      {/* <App /> */}
+      <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,5 +1,21 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
+// http://localhost:5173/users/1?name=Alice&age=25
+
 const UserDetailPage = () => {
-  return <p>User</p>;
+  const params = useParams();
+  console.log(params);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams);
+  console.log(searchParams.toString());
+  console.log(searchParams.get("name"));
+  // console.log(setSearchParams);
+
+  const location = useLocation();
+  console.log(location);
+
+  return <p>User {params.id}</p>;
 };
 
 export default UserDetailPage;
